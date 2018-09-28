4ca8b898a45283a297c7d9c7c0bd0e740e8aea8f
Louis Goessling <louis@goessling.com>
2018-09-27 21:12:13 -0500

    Remove peleus code from master - now developed in the feature/frontend branch

--- peleus/entry.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from thetis import ThetisBackend, BackendError
-app = Flask('peleus')
-
-@app.route('/')
-def hello_world():
-    return "Goodbye, World!"
--- peleus/run_devel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export FLASK_APP=entry.py
-export FLASK_ENV=development
-pipenv run flask run
